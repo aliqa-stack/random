44c960f1eddb9b6f8bcc92fdacd82a2c40fd1737
aliqa-stack <alikalmaliki51@gmail.com>
2026-04-20 20:05:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button");
+const p = document.getElementById("p");
+let arr = ["halo", "iwan77", "iqbal", "siregar", "amartukam", "gacor", "zeus", "67", "slot"]
+
+
+
+let shaking = 0.0;
+
+button.onmousedown = () => {
+    shaking = 0.25;
+    p.innerText = arr[Math.floor(Math.random() * arr.length)]
+}
+
+let prevTimestamp = 0;
+function frame (timestamp) {
+    window.requestAnimationFrame(frame);
+    const deltaTime = (timestamp - prevTimestamp)/1000
+    prevTimestamp = timestamp
+
+    if(shaking > 0) {
+        const x = Math.random() * 2 - 1 + 50;
+        const y = Math.random() * 2 - 1 + 50;
+        button.style.left = `${x}%`;
+        button.style.top = `${y}%`;
+        shaking -= deltaTime
+    }
+
+}
+
+window.requestAnimationFrame(frame);
